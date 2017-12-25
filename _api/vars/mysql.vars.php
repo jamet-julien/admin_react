79df65226a39627562a123efcd3ba02774df11e9
julien jamet <jamet.julien@gmail.com>
2017-12-25 16:21:14 +0100

    Update mysql.vars.php

--- _api/vars/mysql.vars.php
+++ _api/vars/mysql.vars.php
@@ -1,13 +1,5 @@
 <?php
-
-if( strpos( $_SERVER['SERVER_NAME'], 'staging') !== false){
-  define('MYSQL_HOST' , 'acxweb-stage.csgdzoghfbdz.us-east-1.rds.amazonaws.com');
-  define('MYSQL_LOGIN', 'acxibd_user');
-  define('MYSQL_PWD'  , 'PQ+d:m6vV5T9TTJgILmC');
-  define('MYSQL_DB'   , 'acxibd_db');
-}else{
-  define('MYSQL_HOST' , 'acxweb-prod-test.csgdzoghfbdz.us-east-1.rds.amazonaws.com');
-  define('MYSQL_LOGIN', 'acxibd_user');
-  define('MYSQL_PWD'  , '5;860wo3vQU;8OL71ONv');
-  define('MYSQL_DB'   , 'acxibd_db');
-}
+  define('MYSQL_HOST',	getenv('MYSQL_HOST'));
+  define('MYSQL_LOGIN',	getenv('MYSQL_LOGIN'));
+  define('MYSQL_PWD',	getenv('MYSQL_PWD'));
+  define('MYSQL_DB',	getenv('MYSQL_DB'));
